6a51492f826b1c9d5e55571c11ece9e5b35370ef
fmfahath <fmfahath.dev@gmail.com>
2023-01-19 22:06:41 +0530

    js : create new elements with class / attributes and add new tasks to the taks list

--- js/app.js
+++ js/app.js
@@ -15,7 +15,27 @@ window.addEventListener('load', ()=>{
             alert("Please fill out the task");
             return;
         }
-    });
+
+         //create a new element to add new tasks
+   
+    const task_el = document.createElement('div');     //create a div with class name="task"
+    task_el.classList.add('task');
+    
+    const task_content_el = document.createElement('div'); //create a div with class name="content"
+    task_content_el.classList.add('content'); 
+
+    const task_input_el = document.createElement('input');
+    task_input_el.classList.add('text');
+    task_input_el.type = 'text';
+    task_input_el.value = task;
+    task_input_el.setAttribute('readonly', 'readonly');
+   
+    list_el.appendChild(task_el);
+    task_el.appendChild(task_content_el);
+    task_content_el.appendChild(task_input_el); 
 
     
+    });
+
+   
 })
