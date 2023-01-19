dc0e5b5a1049242d34a8eaec4ab926f3aaf696f7
fmfahath <fmfahath.dev@gmail.com>
2023-01-19 23:06:03 +0530

    js : delete button function added

--- js/app.js
+++ js/app.js
@@ -65,5 +65,10 @@ window.addEventListener('load', ()=>{
                 task_edit_el.innerText = "Edit";
            }
         })
+
+       //add funtions to Delete button  
+       task_delete_el.addEventListener('click',()=>{
+            list_el.removeChild(task_el);
+       })
     })
 })
