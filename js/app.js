06a8e353ff615c5432b5aa7183ed754a49d65293
fmfahath <fmfahath.dev@gmail.com>
2023-01-19 22:25:34 +0530

    js : create new elements - edit, delete buttons / added to the DOM

--- js/app.js
+++ js/app.js
@@ -16,25 +16,42 @@ window.addEventListener('load', ()=>{
             return;
         }
 
-         //create a new element to add new tasks
-   
-    const task_el = document.createElement('div');     //create a div with class name="task"
-    task_el.classList.add('task');
-    
-    const task_content_el = document.createElement('div'); //create a div with class name="content"
-    task_content_el.classList.add('content'); 
 
-    const task_input_el = document.createElement('input');
-    task_input_el.classList.add('text');
-    task_input_el.type = 'text';
-    task_input_el.value = task;
-    task_input_el.setAttribute('readonly', 'readonly');
-   
-    list_el.appendChild(task_el);
-    task_el.appendChild(task_content_el);
-    task_content_el.appendChild(task_input_el); 
+         //create a new elements to add new tasks
+        const task_el = document.createElement('div');     //create a div with class name="task"
+        task_el.classList.add('task');
+        
+        const task_content_el = document.createElement('div'); //create a div with class name="content"
+        task_content_el.classList.add('content'); 
 
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('text');
+        task_input_el.type = 'text';
+        task_input_el.value = task;
+        task_input_el.setAttribute('readonly', 'readonly');
     
+       //add new elements to the DOM
+        task_el.appendChild(task_content_el);
+        task_content_el.appendChild(task_input_el); 
+
+        //add new elements - Edi, Delete buttons
+        const task_action_el = document.createElement('div');
+        task_action_el.classList.add('actions');
+
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerText = "Edit";
+
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = "Delete";
+
+        task_action_el.appendChild(task_edit_el);
+        task_action_el.appendChild(task_delete_el);
+        task_el.appendChild(task_action_el);
+        list_el.appendChild(task_el); 
+
+        input.value = "";
     });
 
    
